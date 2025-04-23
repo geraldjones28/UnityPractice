6f9311ae20c5cdd89d93fddb30fb17645b535ddc
GeraldJones <gajone12@asu.edu>
2025-04-23 15:55:51 -0700

    Added functionality for all clock arms to respresent current time irl

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,12 +1,18 @@
+using System;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+    
     [SerializeField]
     Transform hoursPivot, minutesPivot, secondsPivot;
 
     void Awake()
     {
-        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30); // 30 degree clockwise rotation around Z-axis
+        // Debug.Log(DateTime.Now);
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * DateTime.Now.Hour); // 30 degree clockwise rotation around Z-axis
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * DateTime.Now.Minute);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * DateTime.Now.Second);
     }
 }
