48a4720509ba66bbdee91443a410603371e8b57a
GeraldJones <gajone12@asu.edu>
2025-04-23 16:12:35 -0700

    Basic Clock project completed

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -8,11 +8,12 @@ public class Clock : MonoBehaviour
     [SerializeField]
     Transform hoursPivot, minutesPivot, secondsPivot;
 
-    void Awake()
+    void Update()
     {
+        TimeSpan time = DateTime.Now.TimeOfDay;
         // Debug.Log(DateTime.Now);
-        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * DateTime.Now.Hour); // 30 degree clockwise rotation around Z-axis
-        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * DateTime.Now.Minute);
-        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * DateTime.Now.Second);
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours); // 30 degree clockwise rotation around Z-axis
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
     }
 }
