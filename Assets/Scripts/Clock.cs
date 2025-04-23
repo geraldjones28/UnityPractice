db91970fb50879cdcd03b44617be52744276dbf7
GeraldJones <gajone12@asu.edu>
2025-04-23 15:45:58 -0700

    Set hour arm on clock to 1 hour via quarternion.localRotation function

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
-public class Clock
+public class Clock : MonoBehaviour
 {
-    
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    void Awake()
+    {
+        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30); // 30 degree clockwise rotation around Z-axis
+    }
 }
